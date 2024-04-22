d794f3d44164613b79a5ba9cb6ec146e07f50e83
Cédric Mendoza <cedric@mendoz.fr>
2024-04-22 15:03:12 +0200

    change back url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
 export const config = {
-  api_url: "http://localhost:5000",
+  // render
+  api_url: "https://simplon-ct-back.onrender.com",
 };
+
+/*
+  // local
+  api_url: "http://localhost:5000",
+
+
+  // render
+  api_url: "https://simplon-ct-back.onrender.com",
+*/
